84d6e973559f9431c35181f6bed99bfb5f4087b6
Delta Student <mehtabalam6151@gmail.com>
2025-05-20 06:01:28 +0530

    first commit

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  const listingsWithOwner = initData.data.map((obj) => ({ ...obj, owner: "6820e3467ea6979431b439c5" }));
+  await Listing.insertMany(listingsWithOwner);
+  console.log("data was initialized");
+};
+
+initDB();
